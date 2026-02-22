8e157ba5da624739084d9663393f3a8a99131870
Sara Carri <saracarritome@gmail.com>
2026-02-22 13:42:04 +0000

    Calculator class refactored

--- src/main/java/es/ulpgc/Calculator.java
+++ src/main/java/es/ulpgc/Calculator.java
@@ -1,20 +1,30 @@
 package es.ulpgc;
 
 public class Calculator {
-    public double calculate(String operation, double a, double b) {
-            if (operation.equals("add")) {
-                return a + b;
-            } else if (operation.equals("subtract")) {
-                return a - b;
-            } else if (operation.equals("multiply")) {
-                return a * b;
-            } else if (operation.equals("divide")) {
-                if (b != 0) {
-                    return a / b;
-                } else {
-                    throw new IllegalArgumentException("Cannot divide by zero");
-                }
-            }
-            throw new IllegalArgumentException("Unknown operation: " + operation);
+    public double calculateSum(Operation operation, double a, double b) {
+        if (operation == Operation.ADD) {
+            return a + b;
         }
     }
+
+    public double calculateSubtraction(Operation operation, double a, double b) {
+        if (operation == Operation.SUBTRACT) {
+            return a - b;
+        }
+    }
+
+    public double calculateMultiplication(Operation operation, double a, double b) {
+        if (operation == Operation.MULTIPLY) {
+            return a * b;
+        }
+    }
+
+    public double calculateDivision(Operation operation, double a, double b) {
+        if (operation == Operation.DIVIDE) {
+            if (b != 0) {
+                return a / b;
+            } else {
+                throw new IllegalArgumentException("Cannot divide by zero");
+            }
+    }
+}
---
+++ src/main/java/es/ulpgc/Operation.java
@@ -0,0 +1,5 @@
+package es.ulpgc;
+
+public enum Operation {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
+}
